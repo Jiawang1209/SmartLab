9d8a7a38880c7051ba7bbd327eab8b5f80d22613
Jiawang1209 <Jiawang1209@163.com>
2025-11-26 23:22:28 +0800

    update

---
+++ app.py
@@ -0,0 +1,95 @@
+import dash
+from dash import html
+import feffery_antd_components as fac
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+
+    [
+        fac.AntdRadioGroup(
+            id='space-align-demo-align',
+            options=['start', 'end', 'center', 'baseline'],
+            value='start',
+        ),
+        fac.AntdParagraph('水平方向：'),
+        fac.AntdSpace(
+            [
+                html.Div(
+                    style={
+                        'backgroundColor': 'rgba(64, 173, 255, 1)',
+                        'height': '50px',
+                        'width': '50px',
+                    }
+                ),
+                html.Div(
+                    style={
+                        'backgroundColor': 'rgba(0, 146, 255, 1)',
+                        'height': '25px',
+                        'width': '50px',
+                    }
+                ),
+                html.Div(
+                    style={
+                        'backgroundColor': 'rgba(64, 173, 255, 1)',
+                        'height': '10px',
+                        'width': '50px',
+                    }
+                ),
+            ],
+            id='space-align-demo-horizontal',
+            align='start',
+            style={
+                'backgroundColor': 'rgba(241, 241, 241, 0.6)',
+                'padding': '10px',
+            },
+        ),
+        fac.AntdParagraph('竖直方向：'),
+        fac.AntdSpace(
+            [
+                html.Div(
+                    style={
+                        'backgroundColor': 'rgba(64, 173, 255, 1)',
+                        'height': '50px',
+                        'width': '50px',
+                    }
+                ),
+                html.Div(
+                    style={
+                        'backgroundColor': 'rgba(0, 146, 255, 1)',
+                        'height': '50px',
+                        'width': '25px',
+                    }
+                ),
+                html.Div(
+                    style={
+                        'backgroundColor': 'rgba(64, 173, 255, 1)',
+                        'height': '50px',
+                        'width': '50px',
+                    }
+                ),
+            ],
+            id='space-align-demo-vertical',
+            align='start',
+            direction='vertical',
+            style={
+                'backgroundColor': 'rgba(241, 241, 241, 0.6)',
+                'padding': '10px',
+            },
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output('space-align-demo-horizontal', 'align'),
+    Output('space-align-demo-vertical', 'align'),
+    Input('space-align-demo-align', 'value'),
+)
+def update_demo_align(value):
+    return value, value
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
